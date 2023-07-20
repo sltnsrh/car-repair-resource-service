70e39bb4e2e0c9944e2709659094c3e5b7bc4786
Serhii Salatin <lifenjoy8888@gmail.com>
2023-07-20 12:48:37 +0300

    repositories changed to reactive

--- src/main/java/com/salatin/resource/repository/PartRepository.java
+++ src/main/java/com/salatin/resource/repository/PartRepository.java
@@ -1,9 +1,9 @@
 package com.salatin.resource.repository;
 
 import com.salatin.resource.model.Part;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.elasticsearch.repository.ReactiveElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PartRepository extends ElasticsearchRepository<Part, Long> {
+public interface PartRepository extends ReactiveElasticsearchRepository<Part, Long> {
 }
--- src/main/java/com/salatin/resource/repository/WorkRepository.java
+++ src/main/java/com/salatin/resource/repository/WorkRepository.java
@@ -1,9 +1,9 @@
 package com.salatin.resource.repository;
 
 import com.salatin.resource.model.Work;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.elasticsearch.repository.ReactiveElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WorkRepository extends ElasticsearchRepository<Work, Long> {
+public interface WorkRepository extends ReactiveElasticsearchRepository<Work, Long> {
 }
