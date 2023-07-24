56ea59c3940d2b66effd72716a22a1e2e3828f09
Serhii Salatin <lifenjoy8888@gmail.com>
2023-07-24 18:19:45 +0300

    Reactive elasticsearch config was fixed

---
+++ src/main/java/com/salatin/resource/config/ElasticConfig.java
@@ -0,0 +1,9 @@
+package com.salatin.resource.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.repository.config.EnableReactiveElasticsearchRepositories;
+
+@Configuration
+@EnableReactiveElasticsearchRepositories
+public class ElasticConfig {
+}
